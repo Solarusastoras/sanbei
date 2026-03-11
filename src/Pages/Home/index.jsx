98abfd58aada3e3bfc079bede5efe50f0566af25
Solarus <micformweb@gmail.com>
2026-03-11 03:00:57 +0100

    fix: remove unused supabase import in Home

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Carousel from '../../Composants/Carousel';
 import './home.scss';
-import { supabase } from '../../supabase'; 
 
 function Home() {
   return (
